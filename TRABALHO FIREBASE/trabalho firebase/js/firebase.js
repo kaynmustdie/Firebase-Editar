e78e01a5866dff1bce5d519f445d6fc38a3a8c81
kaynmustdie <alexisonfire.idk@gmail.com>
2026-05-27 13:49:06 -0300

    Add files via upload

---
+++ TRABALHO FIREBASE/trabalho firebase/js/firebase.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCz8BWfIepeTz0fE4SPG_A1H5ifjOpSM9U",
+  authDomain: "jqueryds1504.firebaseapp.com",
+  databaseURL: "https://jqueryds1504-default-rtdb.firebaseio.com",
+  projectId: "jqueryds1504",
+  storageBucket: "jqueryds1504.firebasestorage.app",
+  messagingSenderId: "461244408575",
+  appId: "1:461244408575:web:e2dd5fc5a0359b017462ca",
+  measurementId: "G-BK3NJJ8YR9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
